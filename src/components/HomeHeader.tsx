cb5f313fc080c6193082031f932792f5b034e89c
Pedro Henrique da Silva <pedro_rike@hotmail.com.br>
2022-11-15 17:02:42 -0300

    feat: create home header component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,9 +1,23 @@
-import { Heading, HStack, Text, VStack } from "native-base";
+import { Heading, HStack, Text, VStack, Icon } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
+  const urlImage =
+    "https://static.wikia.nocookie.net/naruto-pedia/images/e/ea/Naruto.png/revision/latest?cb=20120407114822&path-prefix=pt-br";
+
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
-      <VStack>
+      <UserPhoto
+        size={16}
+        source={{
+          uri: urlImage,
+        }}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+      <VStack flex={1}>
         <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
@@ -11,6 +25,10 @@ export function HomeHeader() {
           Pedro
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
     </HStack>
   );
 }
